268cad8efced963d6848fd270c3b7845d796b435
Aadity Raj <avv30682@gmail.com>
2026-03-17 17:19:11 -0700

    Update api.js

--- front/src/utils/api.js
+++ front/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://technical-asigment.onrender.com",
+  baseURL: "https://technical-asigment.onrender.com/api",
   withCredentials: true, 
  })
 
