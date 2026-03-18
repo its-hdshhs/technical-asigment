ff8f44e7e63126c5b0708c37dd0704e74cb370c3
Aadity Raj <avv30682@gmail.com>
2026-03-17 17:07:28 -0700

    adding cors origin

--- back/index.js
+++ back/index.js
@@ -9,7 +9,7 @@ const app = express()
 
 
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://technical-asigment.vercel.app/", 
   credentials: true
 }));
 app.use(express.json());
